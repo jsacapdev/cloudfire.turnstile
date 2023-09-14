c06a22998b4f5c0becc94e81bc39d8cd8d9bc4ba
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2023-09-14 15:24:47 +0100

    ccce1c0a-af71-49de-adfd-ee014e7725b8

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Cloudfire Turnstile
+
+An implementation of [Turnstile](https://developers.cloudflare.com/turnstile/) using ASP.NET Core.
+
+In summary, an approach to address traffic coming to your site from bots avoiding [ever showing a visual puzzle to a user](https://www.troyhunt.com/fighting-api-bots-with-cloudflares-invisible-turnstile/).
---
+++ src/ctwebapp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ctwebapp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        try
+        {
+            // this all needs to be client side, so there is no need to process the request in Azure
+            // and the person attempting to abuse my API gets a nice speedy response directly from an edge node near them
+            string debug = string.Empty;
+
+            var form = await Request.ReadFormAsync();
+
+            var token = form["cf-turnstile-response"];
+
+            var ip = Request.HttpContext.Connection.RemoteIpAddress!.ToString();
+
+            var formData = new MultipartFormDataContent();
+
+            var SECRET_KEY = "1x0000000000000000000000000000000AA";
+
+            formData.Add(new StringContent(SECRET_KEY), "secret");
+            formData.Add(new StringContent(token!), "response");
+            formData.Add(new StringContent(ip), "remoteip");
+
+            var url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+
+            var client = new HttpClient();
+
+            var response = await client.PostAsJsonAsync(url, formData);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                debug += JsonSerializer.Serialize(response);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        return Page();
+    }
+}
+
+
